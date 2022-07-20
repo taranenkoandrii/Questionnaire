66352600dc1db32225f59944bed6a11aca9e22ba
Bodgen <bohdan.garashchuk@gmail.com>
2022-07-20 16:03:43 +0300

    added edit and create pages layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MaterialModule} from "./material.module";
 import {QuestionCreateComponent} from "./pages/question-create/question-create.component";
+import {QuestionEditComponent} from "./pages/question-edit/question-edit.component";
+import {QuestionListComponent} from "./pages/question-list/question-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionCreateComponent
+    QuestionCreateComponent,
+    QuestionEditComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +23,5 @@ import {QuestionCreateComponent} from "./pages/question-create/question-create.c
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/pages/question-edit/question-edit.component.ts
+++ src/app/pages/question-edit/question-edit.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
-  selector: 'question-create',
-  templateUrl: './question-create.component.html',
-  styleUrls: ['./question-create.component.css']
+  selector: 'question-edit',
+  templateUrl: './question-edit.component.html',
+  styleUrls: ['./question-edit.component.css']
 })
-export class QuestionCreateComponent {
+export class QuestionEditComponent {
   typeOfQuestion!: string;
   typesOfQuestion: string[] = ['Single choice', 'Multiply choice', 'Open'];
   value = 'Clear me';
